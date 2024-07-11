541e8b4e9f3e645b5ee7a008ded78419eca94a09
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-07-11 12:24:57 +0530

    Solution to 1365C ; Did some naming conventions and all

---
+++ 1365C_Rotation_Matching.cpp
@@ -0,0 +1,44 @@
+// THE BASIC SNIPPET
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int>a(n),b(n);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<n;i++)
+        cin>>b[i];
+    unordered_map<int,int>aloc, bloc, sol;
+    for(int i=0;i<n;i++){
+        aloc[a[i]] = i;
+        bloc[b[i]] = i;
+    }
+
+    for(int i=0;i<n;i++){
+        int diff = aloc[b[i]] - i;
+        if(diff < 0)
+            diff = n + diff;
+        sol[diff]++;
+    }
+    int ans = 0;
+    for(auto it : sol){
+        if(it.second > ans)
+            ans = it.second;
+    }
+    
+
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    t=1;
+    while(t--)
+        solve();
+    return 0;
+}
--- rough.cpp
+++ rough.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+int main(){
+    unordered_map<int,int>check;
+    check[1] = 1;
+    check[2] = 2;
+    check[3] = 3;
+    check[4] = 4;
+    check[5] = 5;
+    for(auto it:check)  
+        cout<<it.first<<" "<<it.second<<endl;
     return 0;
 }
