a76c7cc3c9f90afeb8e85524d16738c15690f2ef
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-07-13 11:32:38 +0530

    Incomplete solution

---
+++ 1355D_Game_with_array.cpp
@@ -0,0 +1,22 @@
+// THE BASIC SNIPPET
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int N,S;
+    cin>>N>>S;
+
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    // cin >> t;/
+    t=1;
+    while(t--)
+        solve();
+    return 0;
+}
