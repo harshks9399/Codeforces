373813e9dddc667be379f0ba0ecfc83c3bd057ae
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-07-31 22:45:05 +0530

    gg

--- cses/graph_algorithms/round_trip.cpp
+++ cses/graph_algorithms/round_trip.cpp
@@ -3,7 +3,32 @@
 using namespace std;
 #define lli long long int
 
-void dfs(int index, vector<int>&check_all, vector<int>&check, map<int,vector<int>>&edge, stack<int>&progress, int par){
+stack<int>ans_st;
+
+void dfs(int index, vector<int>&check_all, vector<int>&check, map<int,vector<int>>&edge, stack<int>&progress, int par, int &ans){
+    if(ans) return;
+    check_all[index] = 1;
+    check[index] = 1;
+    for(int it:edge[index]){
+        if(it==par) continue;
+        if(check[it]==1){
+            // cycle found
+            ans = 1;
+            progress.push(it);
+            ans_st = progress;
+            // cout<<"HERE\n"<<it<<" "<<par<<"\n";
+            // while(progress.size()!=0){
+            //     cout<<progress.top()<<" ";
+            //     progress.pop();
+            // }
+            // cout<<endl;
+            return;
+        }
+        progress.push(it);
+        dfs(it, check_all, check, edge, progress, index, ans);
+        if(ans) return;
+        progress.pop();
+    }
 
     
 
@@ -30,12 +55,29 @@ void solve(){
     for(int i=1;i<=n;i++){
         if(ans==1) break;
         if(check_all[i]==1) continue;
-
-        
-        dfs(i, check_all, check, edge, progress,-1 );
-
-
+        while(progress.size()!=0) progress.pop();
+        progress.push(i);
+        dfs(i, check_all, check, edge, progress,-1,ans);
+        if(ans!=0) break;
     }
+
+    if(ans==0){
+        cout<<"IMPOSSIBLE\n";
+        return ;
+    }
+
+    unordered_set<int>num_check;
+    vector<int>op;
+    while(1){
+        int top = ans_st.top();
+        ans_st.pop();
+        op.push_back(top);
+        if(num_check.find(top)==num_check.end()) num_check.insert(top);
+        else break;
+    }
+    cout<<op.size()<<"\n";
+    for(int it:op) cout<<it<<" ";
+    return;
 }
 
 int main()
