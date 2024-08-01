13949165ec400b1f7cbdabfccd75ad409a319e58
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-08-01 15:24:43 +0530

    gg

---
+++ template_practice/fenwick_tree.cpp
@@ -0,0 +1,52 @@
+// THE BASIC SNIPPET
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+
+class fenwick_tree{
+    int n_;
+    vector<int>v;
+
+    public :
+    fenwick_tree(int n){
+        n_ = n;
+        v = vector<int>(n+1,0);
+    }
+
+    void update (int index, int add){ // adding sum to the given index and the other indices as needed
+        // v[index] += add;
+        for(;index<=n_;  index = index + (index & (-1 * index)))
+            v[index] += add;
+
+        // i = i + (i * -i)
+    }   
+
+    int sum_1(int index){
+        int ans = 0;
+        for(; index >=1 ; index = index - (index & (-1 * index)))
+            ans += v[index];
+        return ans;
+    }
+    
+    int sum_2(int left, int right){ // right inclusive , left exclusive
+    if(left == 0) return sum_1(right);
+        int ans = sum_1(right) -  sum_1(left);
+        return ans;
+    }
+
+};
+
+
+
+
+void solve(){
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+        solve();
+    return 0;
+}
