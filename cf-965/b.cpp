b06740f1488e449251b161a4cc7417e6f9980ea4
Harsh Kumar Singh <harsh.singh@iitg.ac.in>
2024-08-11 00:44:00 +0530

    gg

--- cf-965/b.cpp
+++ cf-965/b.cpp
@@ -10,34 +10,13 @@ void solve(){
     vector<int>og(n);
     for(int i=0;i<n;i++) cin>>og[i];
     
-    int n1 = n/2 + (n%2);
-    int n2 = n/2;
-    vector<int>a1(n1), a2(n2);
-    for(int i=0;i<n;i++){
-
-        if(i < n2) a2[i] = og[i];
-        else a1[i-n2] = og[i];
-    } 
-
-
-    // int mid = n1;
-    // for(int i=mid;i<n;i++){
-    //     a2[i - mid] = og[i];
-    // }
-    // for(int i=0;i<mid;i++) a1[i] = og[i];
-    reverse(a1.begin(), a1.end());
-    reverse(a2.begin(), a2.end());
-
-    if(n%2==1){
-        int l1 = a1[0], l2 = a1[n1-1];
-        a1[n1-1] = l1;
-        a1[0] = l2;
+    vector<int>ans(n);
+    ans[n-1] = og[0];
+    for(int i=1;i<n;i++){
+        ans[i-1] = og[i];
     }
-
-    for(int it:a1) cout<<it<<" ";
-    for(int it:a2) cout<<it<<" ";
-
-    cout<<"\n";
+    for(int it:ans) cout<<it<<" ";
+    cout<<" ";
     return;
     
 
